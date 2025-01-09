7e91796ed4465e13774d9f49c5b077501854bb74
llddang <bodyness@naver.com>
2025-01-09 15:47:37 +0900

    feat: movie container 클릭시 모달이 열리도록 구현
    #3

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
+import { MovieDetailModal } from "../components/ui/movieDetailModal.js";
 import { getPopularMovieList } from "../lib/api/movie.api.js";
+import { BookmarkType } from "../types/bookmark.type.js";
 
 const movieContainer = document.getElementById("movie-container");
 
@@ -6,10 +8,12 @@ await getPopularMovieList().then((res) => {
   res.results.forEach((movie) => {
     const item = document.createElement("li");
     item.setAttribute("class", "movie-item");
-    item.setAttribute("key", movie.id);
+    item.setAttribute("data-id", movie.id);
+    item.setAttribute("data-content", JSON.stringify(movie));
     item.innerHTML = `
       <img
         src="https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}"
+        alt="${movie.title} 포스터"
       />
       <div class="movie-content">
         <p>${movie.title}</p>
@@ -19,3 +23,14 @@ await getPopularMovieList().then((res) => {
     movieContainer.appendChild(item);
   });
 });
+
+movieContainer.addEventListener("click", handleMovieContainerClick);
+function handleMovieContainerClick(e) {
+  const movieItem = e.target.closest(".movie-item");
+  if (!movieItem) return;
+
+  const contentStr = movieItem.getAttribute("data-content");
+  const content = JSON.parse(contentStr);
+
+  new MovieDetailModal(content, BookmarkType.ADD);
+}
